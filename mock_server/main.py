a14716f852a0ae1b8a4975a907a7b86d535f0550
Gia Nguyen <nnhoanggia.garvin@gmail.com>
2025-05-02 12:02:17 +0700

    update service port

--- mock_server/main.py
+++ mock_server/main.py
@@ -14,3 +14,8 @@ app.add_middleware(
 
 app.include_router(products.router)
 app.include_router(filters.router)
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
